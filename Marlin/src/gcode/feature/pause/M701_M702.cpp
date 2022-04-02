637201ba383b35f4d0a1e9d7c00cfc7177484424
andremoraisp <andremoraisp@yahoo.com.br>
2022-04-02 15:16:23 -0300

    Ajuste de troca de filamento - Não move Z se não for necessário

--- Marlin/src/gcode/feature/pause/M701_M702.cpp
+++ Marlin/src/gcode/feature/pause/M701_M702.cpp
@@ -193,7 +193,7 @@ void GcodeSuite::M702() {
 
   // Lift Z axis
   if (park_point.z > 0)
-    do_blocking_move_to_z(_MIN(current_position.z + park_point.z, Z_MAX_POS), feedRate_t(NOZZLE_PARK_Z_FEEDRATE));
+    do_blocking_move_to_z(_MIN(_MAX(current_position.z + NOZZLE_PARK_Z_RAISE_MIN, park_point.z), Z_MAX_POS), feedRate_t(NOZZLE_PARK_Z_FEEDRATE));
 
   // Unload filament
   #if HAS_PRUSA_MMU2
